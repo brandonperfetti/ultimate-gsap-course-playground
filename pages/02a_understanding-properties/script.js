49a05a78d84fe11a6a774051a08a8b947112123b
Brandon Perfetti <brandon@brandonperfetti.com>
2025-06-30 10:08:20 -0700

    Complete 02a_understanding-properties.

---
+++ pages/02a_understanding-properties/script.js
@@ -0,0 +1,15 @@
+import gsap from 'gsap';
+
+gsap.to('.box', {
+  opacity: 1,
+  rotation: 360,
+  background: '#ff6f61',
+  scale: 1.25,
+  duration: 2,
+  ease: 'bounce',
+  repeat: -1,
+  yoyo: true,
+  repeatDelay: 2,
+//   paused: true,
+//   stagger: 1,
+});
